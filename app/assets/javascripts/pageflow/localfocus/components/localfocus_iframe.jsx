14df252331eec166a29f6243d7ec729b2b1dd176
Joost Baaij <joost@spacebabies.nl>
2017-12-11 16:57:49 +0100

    use correct syntax for inline css
    thank you @tf

--- app/assets/javascripts/pageflow/localfocus/components/localfocus_iframe.jsx
+++ app/assets/javascripts/pageflow/localfocus/components/localfocus_iframe.jsx
@@ -6,7 +6,7 @@
           className="localfocusvisual"
           scrolling="no"
           frameborder="0"
-          style="width:100%;height:550px;overflow:hidden"
+          style={{width: '100%', height: '550px', overflow: 'hidden'}}
           ref={iframe => this.iframe = iframe}
           src="https://localfocus2.appspot.com/551a9626918b3?api=1">
         </iframe>
