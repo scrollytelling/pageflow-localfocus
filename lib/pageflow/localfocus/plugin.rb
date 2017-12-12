32296a8d3fb2c144e430ae44efb92f7358f8a8fe
Joost Baaij <joost@spacebabies.nl>
2017-12-12 21:14:24 +0100

    added Help Entries
    getting pretty close to release!

--- lib/pageflow/localfocus/plugin.rb
+++ lib/pageflow/localfocus/plugin.rb
@@ -3,6 +3,8 @@ module Pageflow
     class Plugin < Pageflow::Plugin
       def configure(config)
         config.features.register(PageTypeFeature.new(Localfocus.page_type))
+
+        config.help_entries.register('pageflow.localfocus.help_entries.page_type', priority: 49)
       end
     end
   end
