40fbfc7224ddea042499b8426a38c7366399191e
Joost Baaij <joost@spacebabies.nl>
2017-12-12 15:25:33 +0100

    wrap this thing we made in a feature

--- lib/pageflow/localfocus/plugin.rb
+++ lib/pageflow/localfocus/plugin.rb
@@ -2,7 +2,7 @@ module Pageflow
   module Localfocus
     class Plugin < Pageflow::Plugin
       def configure(config)
-        config.page_types.register(Pageflow::Localfocus.page_type)
+        config.features.register(PageTypeFeature.new(Localfocus.page_type))
       end
     end
   end
