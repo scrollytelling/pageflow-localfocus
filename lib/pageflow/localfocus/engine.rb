391e62b80dcb2b542aeab6bbbf8b91f43ebb69ef
Joost Baaij <joost@spacebabies.nl>
2017-12-12 22:01:20 +0100

    properly configure rspec

--- lib/pageflow/localfocus/engine.rb
+++ lib/pageflow/localfocus/engine.rb
@@ -5,6 +5,10 @@ module Pageflow
 
       config.autoload_paths << File.join(config.root, 'lib')
 
+      config.generators do |g|
+        g.test_framework :rspec
+      end
+
       # This has been fixed in newer versions of react-rails.
       initializer "pageflow-localfocus.add_watchable_files", group: :all do |app|
         app.config.watchable_files.concat Dir["#{config.root}/app/assets/javascripts/**/*.jsx*"]
--- spec/pageflow/localfocus_spec.rb
+++ spec/pageflow/localfocus_spec.rb
@@ -1,9 +1,7 @@
+require 'spec_helper'
+
 RSpec.describe Pageflow::Localfocus do
   it "has a version number" do
     expect(Pageflow::Localfocus::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
